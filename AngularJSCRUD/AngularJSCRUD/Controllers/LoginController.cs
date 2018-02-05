8de7105fb40d7571002f3272926710034579e906
Jeremy Binuya <jeremyrbinuya@gmail.com>
2018-02-05 10:32:55 +0800

    added user login

---
+++ AngularJSCRUD/AngularJSCRUD/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularJSCRUD.Models;
+
+namespace AngularJSCRUD.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private EmployeeDBHandle db = new EmployeeDBHandle();
+
+        [HttpPost]
+        public ActionResult LoginUser(EmployeeModel obj)
+        {
+            var user = db.GetEmployee().Where(x => x.Username.Equals(obj.Username) && x.Password.Equals(obj.Password)).SingleOrDefault();
+            return new JsonResult { Data = user, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        
+    }
+}
---
+++ AngularJSCRUD/AngularJSCRUD/Templates/Login/loginapp.js
@@ -0,0 +1,53 @@
+﻿var app = angular.module('loginApp', []);
+
+app.factory('loginService', function ($http) {
+    //Initialize factory Object.
+    var fact = {};
+
+    fact.getUserDetails = function (d) {
+        debugger;
+        return $http({
+            url: '/Login/LoginUser',
+            method: 'Post',
+            data: JSON.stringify(d),
+            headers: { 'content-type': 'application/json' }
+        });//.then(function (response) {
+        //    window.location.href = '#/Employees';
+        //});
+    };
+
+    return fact;
+});
+
+app.controller('loginController', function ($scope, loginService) {
+
+    $scope.loginData = {
+        Username: '',
+        Password: '',
+    }
+    $scope.msg = "";
+    $scope.Submitted = false;
+    $scope.IsLoggedIn = false;
+    $scope.IsFormValid = false;
+
+    $scope.$watch("myForm.$valid", function (TrueOrFalse) {
+        $scope.IsFormValid = TrueOrFalse;
+    });
+
+    $scope.LoginForm = function () {
+        $scope.Submitted = true;
+        if ($scope.IsFormValid) {
+            loginService.getUserDetails($scope.UserModel).then(function (d) {
+                debugger;
+                if (d.data.Username != null) {
+                    debugger;
+                    $scope.IsLoggedIn = true;
+                    //$scope.msg = "You Successfully Logged Mr/Ms " + d.data.FullName;
+                    window.location.href = '#/Employees';
+                } else {
+                    alert("Invalid credentials buddy! Try Again");
+                }
+            });
+        }
+    }
+});
